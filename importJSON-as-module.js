d9cd99e39a5a483525a7950a104ec35e49823960
Alan Lansdowne <alan_lansdowne@yahoo.com>
2020-11-16 01:16:32 +0000

    Create importJSON-as-module.js

---
+++ importJSON-as-module.js
@@ -0,0 +1,102 @@
+const ashiva = (() => {
+
+
+    //*******************************//
+   // importJSON :: PRIVATE OBJECTS //
+  //*******************************//
+
+  const importJSONMethods = {
+
+    getJSONStorageKey: (filepath) => {
+
+      let jsonStorageKey = 'importJSON::' + src(filepath.replace('/.assets/', '').replace('.json', ''));
+
+      return jsonStorageKey;
+    },
+
+
+    getFilepath: (jsonStorageKey) => {
+
+      let filepath = '/.assets/' + url(jsonStorageKey.replace('importJSON::', '')) + '.json';
+
+      return filepath;
+    },
+
+
+    fetchJSON: async (jsonStorageKey) => {
+
+      const filepath = importJSONMethods.getFilepath(jsonStorageKey);
+
+      const response = await fetch(filepath);
+      const importedJSON = await response.text();
+      await localStorage.setItem(jsonStorageKey, importedJSON);
+      await localStorage.setItem(jsonStorageKey + '::Timestamp', Date.now());
+
+      const jsonStoredEvent = new Event('storeJSON');
+      await window.dispatchEvent(jsonStoredEvent);
+    }
+  };
+
+
+  const importJSONProperties = {
+
+    timeLimit: (1000 * 60) // (1000 * 60 * 60 * 24 * 7)
+  };
+
+
+
+    //******************************//
+   // importJSON :: PUBLIC OBJECTS //
+  //******************************//
+
+  const importJSON = (filepath, callback, parameters = {}) => {
+
+    const jsonStorageKey = importJSONMethods.getJSONStorageKey(filepath);
+    const storedJSON = localStorage.getItem(jsonStorageKey);
+    const timeStamp = localStorage.getItem(jsonStorageKey + '::Timestamp');
+    const timeElapsed = (Date.now() - timeStamp);
+
+    // RETRIEVE JSON FROM LOCALSTORAGE (IF ENTRY & TIME LIMIT PERMIT) 
+    if ((storedJSON !== null) && (timeElapsed < importJSONProperties.timeLimit)) {
+
+      return callback(storedJSON, parameters);
+    }
+
+    // ELSE FETCH JSON
+    else {
+
+      importJSONMethods.fetchJSON(jsonStorageKey);
+
+      window.addEventListener('storeJSON', () => {
+
+        const newStoredJSON = localStorage.getItem(jsonStorageKey);
+
+        parameters.fetched = true;
+
+        return callback(newStoredJSON, parameters);
+      });
+    }
+  };
+
+
+  return { importJSON };
+
+})();
+
+
+
+// WORKING EXAMPLE:
+
+const buildParagraphParameters = {color: 'rgb(0, 125, 0)'};
+
+function buildParagraph(JSON, parameters) {
+
+  let paragraph = document.createElement('p');
+  parameters.color = (parameters.fetched) ? 'rgb(255, 0, 0)' : parameters.color;
+  paragraph.style.color = parameters.color;
+  paragraph.textContent = JSON;
+
+  document.body.appendChild(paragraph);
+}
+
+ashiva.importJSON('/.assets/content/pages/about-us/page.json', buildParagraph, buildParagraphParameters);
